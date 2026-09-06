389d29d1efd6ba6450d39c2a3a869d7abb1c7044
Sammar Gaikwad <147311645+SammarGaikwad@users.noreply.github.com>
2026-09-07 00:17:42 +0530

    Upload folder py codes

---
+++ py codes/funcs.py
@@ -0,0 +1,17 @@
+def calculate(a ,b):
+    if(a <= 0 or b <= 0):
+        print("Please enter positive numbers only.")
+        return
+    elif(a > b):
+        print("The first number is greater than the second number.")
+        print("The sum of", a, "and", b, "is", a + b)
+        if(a < b):
+            print("The first number is less than the second number.")
+            print("The sum of", a, "and", b, "is", a + b)
+    else:
+        print("The first number is equal to the second number.")
+        print("The sum of", a, "and", b, "is", a + b)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+calculate(a, b)
---
+++ py codes/loops.py
@@ -0,0 +1,13 @@
+'''for i in range(1,12):
+    print("5 x", i, "=", 5 * i)
+    if(i == 10):
+        break
+print("Loop ended at i =", i)'''
+
+i = 0
+while True:
+    print(i)
+    i += 1
+    if(i == 100):
+        break
+print("Loop ended at i =", i)
